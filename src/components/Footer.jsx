98008b8e5db866ddc5590d4c7b380871c4aed222
BenzHoang <benzminh2003@gmail.com>
2024-08-28 23:22:00 +0700

    update footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,8 +3,8 @@ import { Box, Typography } from '@mui/material';
 
 const Footer = () => {
     return (
-        <Box component="Footer" sx={{mt:'auto', p:2, backgroundColor: '#fff' , color: '#000'}}>
-            <Typography align='center'>© Tháng 6 2024 - Student Management App</Typography>
+        <Box component="Footer" sx={{mt:'auto', p:2, backgroundColor: '#a0a0a0' , color: '#000'}}>
+            <Typography align='center'>© Tháng 6 2024 - ArtTools Management App</Typography>
         </Box>
     );
 };
